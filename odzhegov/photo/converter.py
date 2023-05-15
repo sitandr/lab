bb29cd85472fd4c84a32657178566774f097dda4
sitandr <andr-sitnikov@mail.ru>
2023-05-15 20:43:32 +0300

    Odzhegov: photos + calc 3

--- odzhegov/photo/converter.py
+++ odzhegov/photo/converter.py
@@ -8,6 +8,8 @@ for i in os.walk('.'):
     for f in i[2]:
         if not f.endswith('.bmp'):
             continue
+        if os.path.isfile(f[:len('.bmp') + '.png']):
+            continue
         print(f)
         subprocess.call(["magick", p+'/'+i[0] + '/'+f, p+'/'+i[0] + '/'+ f[:-4] + ".png"], stderr=subprocess.PIPE)
         
